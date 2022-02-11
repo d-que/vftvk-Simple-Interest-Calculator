7adf76f1071d74cb2a4017f7a9b1dac3001d41f1
Daniel Quetschlich <it@quetschli.ch>
2022-02-11 17:18:34 +0100

    Update script.js
    Finished JS

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText="If you deposit " + principal + "\,\n at an interest rate of " + rate + "%\.\n You will receive an amount of " + interest + "\,\n in the year " + year + "\." 
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}        
