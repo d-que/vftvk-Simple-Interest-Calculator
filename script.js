d62908c6d65a781e2d8646ebb90026944d40672e
Daniel Quetschlich <it@quetschli.ch>
2022-02-11 18:11:55 +0100

    Update script.js
    corret js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function checkInput()
 {
     //create references to the input elements we wish to validate
-    var principal = parseFloat(document.getElementById("principal").value);
-    principal = Math.round(principal * 100) / 100
-    alert(typeof principal )
+    var principal = document.getElementById("principal");
+    // Get value of amount rounded to 2 digits
+    var princival = Math.round(parseFloat(principal.value) * 100) / 100
 
     //Check if principal field is filled correctly
-    if(principal <= 0)
+    if( (princival <= 0) || (isNaN(princival)))
     {
           alert("Please enter valid amount (i.e. positive number)");
+          principal.focus()
           return false;
     }
    //If all is well return true.
@@ -18,13 +19,14 @@ function checkInput()
 
 function compute()
 {
-    checkInput()
+    if (checkInput()){
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerText="If you deposit " + principal + "\,\n at an interest rate of " + rate + "%\.\n You will receive an amount of " + interest + "\,\n in the year " + year + "\." 
+    }
 }
 function updateRate() 
 {
